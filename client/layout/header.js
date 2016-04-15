f786978e0ce8845746fe212ee63e65fca0f42eaf
Henry Williams <henryw07@gmail.com>
2016-04-15 01:28:22 -0400

    Select item on header to indicate the current path the user is on

--- client/layout/header.js
+++ client/layout/header.js
@@ -6,17 +6,22 @@ import './layout.modules'
 import template from './header.ng.html'
 
 class HeaderController {
-    constructor($scope) {        
+    constructor($scope, $location) { 
         $scope.viewModel(this)
+        this._$location = $location
     }
     
     username() {
         return Meteor.user() && Meteor.user().username
+    }
+    
+    isActive(path) {
+        return this._$location.path() === path
     }
 }
 
 angular.module('booksrus.layout')
 .component('header', {
     templateUrl: 'client/layout/header.ng.html',
-    controller: ['$scope', HeaderController]
+    controller: ['$scope', '$location', HeaderController]
 })
